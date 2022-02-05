eb568e3c0131686f1542253a7dbb341f1fa3c6e5
Michael Kohl <me@citizen428.net>
2022-02-05 15:23:08 +0700

    Update Lualine config

--- lua/statusline.lua
+++ lua/statusline.lua
@@ -1,6 +1,14 @@
-local gps = require("nvim-gps")
+local lualine_ok, lualine = pcall(require, "lualine")
+if not lualine_ok then
+  return
+end
 
-require("lualine").setup({
+local gps_ok, gps = pcall(require, "nvim-gps")
+if not gps_ok then
+  return
+end
+
+lualine.setup({
   sections = {
     lualine_c = {
       { gps.get_location, cond = gps.is_available }

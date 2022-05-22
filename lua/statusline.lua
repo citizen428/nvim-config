c0c3ed8257c9326ca05e4472f556ff9426f3be51
Michael Kohl <me@citizen428.net>
2022-05-22 20:21:44 +0700

    Add custom acme Lualine theme

--- lua/statusline.lua
+++ lua/statusline.lua
@@ -8,11 +8,33 @@ if not gps_ok then
   return
 end
 
+local colors = {
+  bg = "#eaffff",
+  black = "#000000"
+}
+
+local acme = {
+  normal = {
+    a = { fg = colors.black, bg = colors.bg, gui = 'bold' },
+    b = { fg = colors.black, bg = colors.bg },
+    c = { fg = colors.black, bg = colors.bg },
+  },
+  insert = { a = { fg = colors.black, bg = colors.bg, gui = 'bold' } },
+  visual = { a = { fg = colors.black, bg = colors.bg, gui = 'bold' } },
+  replace = { a = { fg = colors.black, bg = colors.bg, gui = 'bold' } },
+  inactive = {
+    a = { fg = colors.black, bg = colors.bg, gui = 'bold' },
+    b = { fg = colors.black, bg = colors.bg },
+    c = { fg = colors.black, bg = colors.bg },
+  },
+}
+
 lualine.setup({
   sections = {
     lualine_c = {
       { gps.get_location, cond = gps.is_available }
     }
   },
-  extensions = {"fugitive", "nvim-tree", "quickfix"}
+  extensions = {"fugitive", "nvim-tree", "quickfix"},
+  options = { theme = acme }
 })
